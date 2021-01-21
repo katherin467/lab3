61c7ca849b524f56084833bf61fde37ff256ef76
Katherine Lim <k4lim@ucsd.edu>
2021-01-21 04:10:07 -0800

    ok cool

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,27 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron p").toggleClass("active");
+		$("#testjs").text("Hello World!");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	 $("a.thumbnail").click(projectClick);
+}
+
+// from lab 2
+function projectClick(e) {
+	// prevent page from reloading
+	e.preventDefault();
+	// this refers to the object that triggered event
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    	$(description).fadeOut();
+    }
 }
